8ba9d380353eb188bd1171c1a6faddadfd980e46
yohan <etudiant@imac-de-admin-6.local>
2019-04-10 10:59:56 +0400

    1er commit

--- ProjetAjournee/GameScene.swift
+++ ProjetAjournee/GameScene.swift
@@ -8,78 +8,120 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
+    //declaration des des objets(joueur et les notes)
+    var note:SKSpriteNode = SKSpriteNode()
+    var player:SKSpriteNode = SKSpriteNode()
+    // declaration des collision
+    let collisionNote : UInt32 = 1
+    let collisionWal : UInt32 = 2
+    let collisionPlayer : UInt32 = 4
+    // declaration de audioPlayer
+    var audioPlayer = AVAudioPlayer()
+    // vecteur pour le deplacement vers le haut du joueur
+    var monter: CGVector = CGVector(dx: 0, dy: 200)
+    // score et nombre de note pris
+    var scoreLabel:SKLabelNode = SKLabelNode()
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var gameTimer:Timer = Timer()
+    
+    var possibleNotes = [""]
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        self.anchorPoint = CGPoint(x: 0, y: 0)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -2)
+        self.physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: CGRect(x: frame.minX , y: frame.minY , width: frame.width , height: frame.height))
+        self.physicsBody?.collisionBitMask = collisionWal
+        
+        
+        let audioFileURL = Bundle.main.url(forResource: "gamra", withExtension: "mp3")
+        do {
+            let sound = try AVAudioPlayer(contentsOf: audioFileURL!)
+            audioPlayer = sound
+            sound.play()
+        } catch {
+            // couldn't load file :(
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let background = SKSpriteNode(imageNamed: "background")
+        background.anchorPoint = CGPoint(x: 0, y: 0)
+        background.position = CGPoint(x: 0, y: 0)
+        background.size.width = frame.size.width
+        background.size.height = frame.size.height
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        background.zPosition = -1
+        
+        self.addChild(background)
+        
+        player = SKSpriteNode(imageNamed : "fille")
+        player.zPosition = 0
+        player.position = CGPoint(x: player.size.width/2, y: player.size.height/2 + 1000)
+        player.anchorPoint = CGPoint(x: 0, y: 0)
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.categoryBitMask = collisionPlayer
+        player.physicsBody?.collisionBitMask = 0
+        player.physicsBody?.contactTestBitMask = 0
+        
+        self.addChild(player)
+        
+        scoreLabel = SKLabelNode(text : "Score : 0")
+        scoreLabel.position = CGPoint(x: 100, y: self.frame.size.height - 60)
+        scoreLabel.fontName = "AmeriacanTypewriter-Bold"
+        scoreLabel.fontColor = UIColor.darkGray
+        score = 0
+        
+        self.addChild(scoreLabel)
+ 
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addNotes), userInfo: nil, repeats: true)
+        
+       
+    }
+    func addNotes() {
+        possibleNotes = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleNotes) as! [String]
+        let note = SKSpriteNode(imageNamed: possibleNotes[0])
+        
+        let randomNotesPosition = GKRandomDistribution(lowestValue: 0, highestValue: 1334)
+        let position = CGFloat(randomNotesPosition.nextInt())
+        
+        note.position = CGPoint(x: self.frame.size.width + note.size.width , y: position)
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+       
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+       
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+       
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+      
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+       
     }
     
     
