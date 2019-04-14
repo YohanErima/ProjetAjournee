e3b6d78b47128d72e5739d1526c74748401844b1
Erima Yohan <y.erima@orange.fr>
2019-04-14 10:08:48 +0400

    GameOver.swift

--- ProjetAjournee/GameOverScene.swift
+++ ProjetAjournee/GameOverScene.swift
@@ -26,6 +26,34 @@ class GameOverScene: SKScene {
         
         ButtonReplay = self.childNode(withName: "ButtonReplay") as! SKSpriteNode
         
+        ButtonSave = self.childNode(withName: "SaveButton") as! SKSpriteNode
+        
+        ButtonAcceuil = self.childNode(withName: "ButtonAcceuil") as! SKSpriteNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let location = touch?.location(in: self){
+            let node = self.nodes(at: location)
+            
+            if node[0].name == "ButtonReplay" {
+                let transition = SKTransition.flipVertical(withDuration: 0.6)
+                let gameScene = GameScene(size: self.size)
+                self.view!.presentScene(gameScene, transition: transition)
+            }
+            if node[0].name == "ButtonSave" {
+                let transition = SKTransition.flipVertical(withDuration: 0.6)
+                let gameScene = GameScene(size: self.size)
+                self.view!.presentScene(gameScene, transition: transition)
+                
+            }
+            if node[0].name == "ButtonAcceuil" {
+                let transition = SKTransition.flipVertical(withDuration: 0.6)
+                let gameScene = GameScene(size: self.size)
+                self.view!.presentScene(gameScene, transition: transition)
+                
+            }
+        }
     }
 
 }
